eb7164060d7a0bd6fd1056c3a01d2dba0c05e8f9
o3u4 <149888279+o3u4@users.noreply.github.com>
2023-12-26 21:55:34 +0800

    improve the answer class
    improve the answer class and make sure every situation has its solution

--- answer.py
+++ answer.py
@@ -1,113 +1,170 @@
-Base = []
+import random
 
 
-def dist(x, y):
-    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+class Answer:
+    Base = []
 
+    def __init__(self, scale, rule):
+        self.scale = scale
+        self.rule = rule
 
-def mat(i):
-    mi = []
-    for x in range(4):
-        for y in range(4):
-            if dist((x, y), (i // 4, i % 4)) <= 2:
-                mi.append(1)
+    @staticmethod
+    def dist(x, y):
+        return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
+    def mat(self, i):
+        mi = []
+        for x in range(self.scale):
+            for y in range(self.scale):
+                if self.dist((x, y), (i // self.scale, i % self.scale)) <= self.rule:
+                    mi.append(1)
+                else:
+                    mi.append(0)
+        return mi
+
+    def gen_mat(self):
+        for item in range(self.scale ** 2):
+            self.Base.append(self.mat(item))
+
+    def add(self, m1, m2):
+        new_m = []
+        for i in range(self.scale ** 2):
+            new_m.append((m1[i] + m2[i]) % 2)
+        return new_m
+
+    def prt(self):
+        for i in range(self.scale ** 2):
+            for j in range(len(self.Base)):
+                print(self.Base[j][i], end=' ')
+                if j == self.scale ** 2 - 1:
+                    print("|", end='')
+            print()
+
+    def row_add(self, i, j, mode=1):
+        # 第i行加到第j行里, mode=1为增广矩阵, 0为系数矩阵
+        if mode == 1:
+            for x in range(self.scale ** 2 + 1):
+                self.Base[x][j] = (self.Base[x][j] + self.Base[x][i]) % 2
+        else:
+            for x in range(self.scale ** 2):
+                self.Base[x][j] = (self.Base[x][j] + self.Base[x][i]) % 2
+
+    def row_change(self, i, j, mode=1):
+        # i, j行交换, mode=1为增广矩阵, 0为系数矩阵
+        if mode == 1:
+            for x in range(self.scale ** 2 + 1):
+                mid = self.Base[x][i]
+                self.Base[x][i] = self.Base[x][j]
+                self.Base[x][j] = mid
+        else:
+            for x in range(self.scale ** 2):
+                mid = self.Base[x][i]
+                self.Base[x][i] = self.Base[x][j]
+                self.Base[x][j] = mid
+
+    def row_simplified(self, mode=1):
+        # 高斯消元法行化简, mode=1为增广矩阵, 0为系数矩阵
+        for j in range(self.scale ** 2):
+            position = []  # 第j列里1的位置
+            for i in range(self.scale ** 2):
+                if self.Base[j][i] == 1:
+                    position.append(i)
+            c_pos = -1  # 第1个大于等于j的非0元
+            for f in position:
+                if f >= j:
+                    c_pos = f
+                    break
+            # print(c_pos)
+            # print(position)
+            if c_pos == -1:
+                continue
+            elif len(position) > 1:
+                for p in range(len(position)):
+                    if position[p] != c_pos:
+                        self.row_add(c_pos, position[p], mode)
+                if c_pos != j:
+                    self.row_change(j, c_pos, mode)
             else:
-                mi.append(0)
-    return mi
+                if c_pos != j:
+                    self.row_change(j, c_pos, mode)
+            # prt()
+            # print('----------------------------------')
 
+    def exam(self):
+        # 检查阶梯型是否满秩
+        flag = True
+        for i in range(self.scale ** 2):
+            if self.Base[i][i] == 0:
+                flag = False
+            else:
+                continue
+        if not flag:
+            # self.prt()
+            # print("不满秩")
+            return False
+        else:
+            return True
 
-def gen_mat():
-    for item in range(4 * 4):
-        Base.append(mat(item))
-
-
-def add(m1, m2):
-    new_m = []
-    for i in range(4 * 4):
-        new_m.append((m1[i] + m2[i]) % 2)
-    return new_m
-
-
-def prt():
-    for i in range(4 * 4):
-        for j in range(4 * 4 + 1):
-            print(Base[j][i], end=' ')
-        print()
-
-
-def row_add(i, j):
-    # 第i行加到第j行里
-    for x in range(4 * 4 + 1):
-        Base[x][j] = (Base[x][j] + Base[x][i]) % 2
-
-
-def row_change(i, j):
-    # i, j行交换
-    for x in range(4 * 4 + 1):
-        mid = Base[x][i]
-        Base[x][i] = Base[x][j]
-        Base[x][j] = mid
-
-
-def row_simplified():
-    # 高斯消元法行化简
-    for j in range(4 * 4):
-        position = []   # 第j列里1的位置
-        for i in range(4 * 4):
-            if Base[j][i] == 1:
-                position.append(i)
-        c_pos = -1      # 第1个大于等于j的非0元
-        for f in position:
-            if f >= j:
-                c_pos = f
+    def det(self):
+        # 判断是否有解
+        det = True
+        for j in range(self.scale ** 2):
+            if self.Base[self.scale ** 2][j] == 1 and self.Base[j][j] == 0:
+                # print("无解")
+                det = False
                 break
-        # print(c_pos)
-        # print(position)
-        if c_pos == -1:
-            continue
-        elif len(position) > 1:
-            for p in range(len(position)):
-                if position[p] != c_pos:
-                    row_add(c_pos, position[p])
-            if c_pos != j:
-                row_change(j, c_pos)
+        return det
+
+    def answer(self, init, target):
+        self.Base = []
+        self.gen_mat()
+        det_s = self.add(init, target)
+        self.Base.append(det_s)
+        self.row_simplified()
+        self.prt()
+        if self.exam() or self.det():
+            asw = []
+            for i in range(self.scale ** 2):
+                if self.Base[self.scale ** 2][i] == 1:
+                    asw.append(i + 1)
+            if self.exam():
+                print("满秩, 必有唯一解")
+            else:
+                print("不满秩, 但有解")
+            return asw
         else:
-            if c_pos != j:
-                row_change(j, c_pos)
-        # prt()
-        # print('----------------------------------')
+            print("无解")
+            return None
 
+    def sol_sys(self):
+        self.Base = []
+        self.gen_mat()
+        seq = [0 for x in range(self.scale ** 2)]
+        for i in range(self.scale ** 2):
+            if random.randint(0, 1) == 1:
+                seq = self.add(seq, self.Base[i])
+        return seq
 
-def exam():
-    # 检查阶梯型是否满秩
-    flag = True
-    for i in range(4 * 4):
-        if Base[i][i] == 0:
-            flag = False
-        else:
-            continue
-    if not flag:
-        return False
-    else:
-        return True
-
-
-def answer(init, target):
-    gen_mat()
-    det_s = add(init, target)
-    Base.append(det_s)
-    row_simplified()
-    if exam():
-        asw = []
-        for i in range(4 * 4):
-            if Base[4 * 4][i] == 1:
-                asw.append(i + 1)
-        return asw
+    @classmethod
+    def sol_list(cls):  # 查看所有情况的满秩解
+        f_sol = []
+        for f_j in range(3, 10):
+            f_s = [f_j]
+            for i in range(1, f_j):
+                f_a = Answer(f_j, i)
+                f_a.Base = []
+                f_a.gen_mat()
+                f_a.row_simplified(0)
+                if f_a.exam():
+                    f_s.append(i)
+            f_sol.append(f_s)
+        print(f_sol)
 
 
 if __name__ == '__main__':
-    org = [0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0]
-    tgt = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    aw = answer(org, tgt)
-    prt()
-    print(aw)
+    # Answer.sol_list()
+    x1 = [random.randint(0, 1) for x in range(4 * 4)]
+    x2 = [random.randint(0, 1) for x in range(4 * 4)]
+    a = Answer(4, 2)
+    Asw = a.answer(x1, x2)
+    print(Asw)
--- lattice.py
+++ lattice.py
@@ -1,48 +1,57 @@
 import tkinter as tk
 import random as rd
-import jgg
+from answer import Answer
 
 
 class Lattice:
-    seq = []    # 01序列
-    txt_list = []   # 转成tk的字符串
-    buttons = []    # 按钮
-    mny = 0         # 尝试次数
+    seq = []  # 01序列
+    txt_list = []  # 转成tk的字符串
+    buttons = []  # 按钮
+    mny = 0  # 尝试次数
     root = tk.Tk()
     root.title('点亮所有格子')
 
-    def __init__(self, scale):
-        self.scale = scale      # 规模
-        self.init_rand()        # 首次初始化
-        self.root.geometry(f"{self.scale}00x{self.scale}00")    # 界面大小
+    def __init__(self, scale, rule=2, mode=0):
+        self.scale = scale  # 规模
+        self.rule = rule    # 点击后多少距离的格子改变
+        self.mode = mode    # 是否保证生成的矩阵有解
+        self.init_rand()    # 首次初始化
+        self.root.geometry(f"{self.scale}00x{self.scale}00")  # 界面大小
 
     def init_rand(self):
-        for odd in range(self.scale ** 2):
-            n = rd.randint(0, 1)
-            self.seq.append(n)
-            t = tk.StringVar()
-            self.txt_list.append(t)
+        if self.mode == 0:
+            for odd in range(self.scale ** 2):
+                n = rd.randint(0, 1)
+                self.seq.append(n)
+                t = tk.StringVar()
+                self.txt_list.append(t)
+        else:
+            init = Answer(self.scale, self.rule)
+            self.seq = init.sol_sys()
+            for odd in range(self.scale ** 2):
+                t = tk.StringVar()
+                self.txt_list.append(t)
 
         for i in range(self.scale ** 2):
             self.txt_list[i].set(str(self.seq[i]))
 
-    def change(self, x, y):     # 点击后改变的规则
+    def change(self, x, y):  # 点击后改变的规则
         for xi in range(self.scale):
             for yj in range(self.scale):
-                if self.dist((x, y), (xi, yj)) <= self.scale - 2:
+                if self.dist((x, y), (xi, yj)) <= self.rule:
                     if self.txt_list[self.scale * xi + yj].get() == '0':
                         self.txt_list[self.scale * xi + yj].set('1')
                     else:
                         self.txt_list[self.scale * xi + yj].set('0')
         self.mny += 1
 
-    def popup(self):    # 提交, 3阶以内可查看答案
+    def popup(self):
         num = 0
         for tx in self.txt_list:
             if tx.get() == '1':
                 num += 1
         up = tk.Toplevel()
-        up.geometry("200x60")
+        up.geometry("400x60")
         label = tk.Label(up)
         if num == self.scale ** 2:
             label.config(text=f'共花了{self.mny}步完成!')
@@ -51,15 +60,16 @@ class Lattice:
             label.config(text='未完成')
             label.pack()
         lab = tk.Label(up)
-        if self.scale == 3:
-            target = [1, 1, 1, 1, 1, 1, 1, 1, 1]
-            ans_list = jgg.answer(self.seq, target)
+        target = [1 for x in range(self.scale ** 2)]
+        asw = Answer(self.scale, self.rule)
+        ans_list = asw.answer(self.seq, target)
+        if ans_list:
             lab.config(text=f'答案为:{", ".join(str(x) for x in ans_list)}')
         else:
-            lab.config(text='我也不知道答案是什么o.O')
+            lab.config(text='无解')
         lab.pack()
 
-    def start_ui(self):     # 启动ui界面
+    def start_ui(self):  # 启动ui界面
         # 使用grid布局排列按钮，并调整行和列的大小
         for i in range(self.scale):
             self.root.rowconfigure(i, weight=1)
@@ -78,25 +88,31 @@ class Lattice:
         bt3.grid(row=self.scale + 1, column=self.scale - 1)
         self.root.mainloop()
 
-    def back(self):     # 回到初始状态
+    def back(self):  # 回到初始状态
         for od in range(self.scale ** 2):
             self.txt_list[od].set(str(self.seq[od]))
         self.mny = 0
 
-    def rand(self):     # 再次随机
+    def rand(self):  # 再次随机
         self.seq = []
         self.mny = 0
-        for _ in range(self.scale ** 2):
-            nub = rd.randint(0, 1)
-            self.seq.append(nub)
+        if self.mode == 0:
+            for _ in range(self.scale ** 2):
+                nub = rd.randint(0, 1)
+                self.seq.append(nub)
+        else:
+            rand = Answer(self.scale, self.rule)
+            self.seq = rand.sol_sys()
         for __ in range(self.scale ** 2):
             self.txt_list[__].set(str(self.seq[__]))
 
     @staticmethod
-    def dist(x, y):     # 定义距离, 用于编写规则
+    def dist(x, y):  # 定义距离, 用于编写规则
         return abs(x[0] - y[0]) + abs(x[1] - y[1])
 
 
 if __name__ == '__main__':
     number = int(input('输入方阵规模(n阶):'))
-    Lattice(number).start_ui()
+    r = int(input("输入规则, 点击后多少距离的格子改变:"))
+    m = int(input("是否保证必定有解(是1, 否0):"))
+    Lattice(number, r, m).start_ui()
